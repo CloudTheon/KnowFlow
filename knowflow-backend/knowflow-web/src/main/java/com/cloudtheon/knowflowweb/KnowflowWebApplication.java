23851c5cdcbf6baf14f37aebbce3720e27aa3dac
cloudtheon <hhhhhg1@outlook.com>
2026-07-27 03:12:22 +0800

    初始化父工程

---
+++ knowflow-backend/knowflow-web/src/main/java/com/cloudtheon/knowflowweb/KnowflowWebApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtheon.knowflowweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowflowWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowflowWebApplication.class, args);
+    }
+
+}
---
+++ knowflow-backend/knowflow-web/src/test/java/com/cloudtheon/knowflowweb/KnowflowWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtheon.knowflowweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowflowWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

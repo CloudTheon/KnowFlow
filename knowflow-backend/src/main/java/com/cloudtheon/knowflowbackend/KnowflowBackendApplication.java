64a3b3b391af65c433f1e46865ecd96a4a909ba9
cloudtheon <hhhhhg1@outlook.com>
2026-07-27 02:23:06 +0800

    初始化后端

---
+++ knowflow-backend/src/main/java/com/cloudtheon/knowflowbackend/KnowflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloudtheon.knowflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowflowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowflowBackendApplication.class, args);
+    }
+
+}
---
+++ knowflow-backend/src/test/java/com/cloudtheon/knowflowbackend/KnowflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudtheon.knowflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowflowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
